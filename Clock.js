5b2444a8a344fa2e253b8b33083b24f355b23a23
DareDevil69 <abdussamad407@gmail.com>
2014-09-07 13:16:13 +0530

    Clockjs

--- Clock.js
+++ Clock.js
@@ -1,1 +1,1 @@
-var timer = setInterval("var d = new Date();var clock = document.getElementById('clock');clock.innerHTML = d;", 1000);
+var timer = setInterval("var d = new Date();var clock = document.getElementById('clock');clock.innerHTML = d;toUTCString", 1000);
